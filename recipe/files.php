cd5b97bcc64c2f52732fa0dd0cd91dcc4a8b339e
Levin Uncu <levin@uncu.de>
2023-06-30 18:31:18 +0200

    Infinite files alias

--- recipe/files.php
+++ recipe/files.php
@@ -11,5 +11,5 @@ task('files:download', static function () {
 desc('Syncs the local files with the remote files');
 task('files:upload', static function () {
   // Upload local files
-  upload("files/", "{{release_or_current_path}}/files/");
+  upload("files/", "{{release_or_current_path}}/files");
 });
